afc30ba24e15e736e2cefc2c935411c5ab71e00e
siraajul <shirajulislamparvez@gmail.com>
2024-11-05 20:11:17 +0600

    Sprint 01:--> Program 01

--- src/Sprint_01/EvenOddNumbers.java
+++ src/Sprint_01/EvenOddNumbers.java
@@ -1,7 +1,19 @@
 package Sprint_01;
 
+import java.util.Scanner;
+
 public class EvenOddNumbers {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+
+        int number = input.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
     }
 }
