3518d849b7dd1f71d68724df78ae0bf7e8bb1fe6
siraajul <shirajulislamparvez@gmail.com>
2024-11-06 07:41:47 +0600

    Sprint 01:--> Program 03

---
+++ src/Sprint_01/LeapYear.java
@@ -0,0 +1,25 @@
+package Sprint_01;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Year: ");
+
+        int year = input.nextInt();
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("Leap Year");
+                } else {
+                    System.out.println("Not Leap Year");
+                }
+            } else {
+                System.out.println("Not Leap Year");
+            }
+        } else {
+            System.out.println("Not Leap Year");
+        }
+    }
+}
